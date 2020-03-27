d7de65372cf37c0b62e961aff6a4e117c75f5025
Leoul1900 <leoulg91@gmail.com>
2020-03-27 15:25:17 +0300

    express static folder and redirect all route to index.html

--- server/index.js
+++ server/index.js
@@ -13,6 +13,19 @@ app.use(cors());
 const posts = require('./routes/api/post')
 
 app.use('/api/posts', posts)
+
+// Handel Production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'))
+    
+    // Handel SPA
+    app.get(/.*/,(req,res)=> res.sendFile(__dirname + '/public/index.html'))
+} else {
+    
+}
+
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
